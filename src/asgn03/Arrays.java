02c55b4b830d1cff6fbcddca37042c4af7a63922
RivkieKlein <43257799+RivkieKlein@users.noreply.github.com>
2018-10-24 10:19:01 -0400

    Empty Methods

---
+++ src/asgn03/Arrays.java
@@ -0,0 +1,25 @@
+package asgn03;
+
+public class Arrays {
+
+	//returns sum of numbers in array
+	public int sum(int[] nums) {
+	return 0;	
+	}
+	
+	//returns max value in array
+	public int max(int[] nums) {
+		return 0;
+	}
+	
+	//returns min value in array
+	public int min(int[] nums) {
+		return 0;
+	}
+	
+	//returns range of nums in array
+	public int range(int[] nums) {
+		return 0;
+	}
+
+}
