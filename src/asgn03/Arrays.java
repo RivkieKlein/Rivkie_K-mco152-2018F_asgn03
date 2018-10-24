de1086bb63a7cb96555b012c7e9321a737343097
RivkieKlein <43257799+RivkieKlein@users.noreply.github.com>
2018-10-24 18:29:15 -0400

    Methods Written-All tests passed

--- src/asgn03/Arrays.java
+++ src/asgn03/Arrays.java
@@ -4,22 +4,74 @@ public class Arrays {
 
 	//returns sum of numbers in array
 	public static int sum(int[] nums) {
-	return 0;	
+	int sum =0;
+	
+	//if array is empty throws exception
+	if(nums.length==0) {
+		throw new EmptyArrayNoNumbers();
+	}
+	
+	//if not empty will loop through array adding
+	else {
+		for(int c =0; c<nums.length; c++) {
+			
+				sum+=nums[c];
+			
+		}
+	}
+	return sum;
 	}
 	
 	//returns max value in array
 	public static int max(int[] nums) {
-		return 0;
+		int max=0;
+		//if array is empty throws exception
+		if(nums.length==0) {
+			throw new EmptyArrayNoNumbers();
+		}
+		
+		//if not empty will loop through array finding max
+		else {
+			max=nums[0];
+			for(int c =0; c<nums.length; c++) {
+				if(nums[c]>max) {
+				max=nums[c];
+				}
+					
+				
+			}
+		}
+		return max;
 	}
 	
 	//returns min value in array
 	public static int min(int[] nums) {
-		return 0;
+		int min=0;
+		//if array is empty throws exception
+		if(nums.length==0) {
+			throw new EmptyArrayNoNumbers();
+		}
+		
+		//if not empty will loop through array finding max
+		else {
+			min=nums[0];
+			for(int c =0; c<nums.length; c++) {
+				if(nums[c]<min) {
+				min=nums[c];
+				}
+					
+				
+			}
+		}
+		return min;
 	}
 	
 	//returns range of nums in array
 	public static int range(int[] nums) {
-		return 0;
+		if(nums.length==0) {
+			throw new EmptyArrayNoNumbers();
+		}
+		return ((max(nums)-min(nums))+1);
 	}
 
 }
